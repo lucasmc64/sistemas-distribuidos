65f3014b3f1af631fc7e5414b2dc9b10be783e21
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2023-08-19 09:56:17 -0300

    Some structs

--- etapa-1/src/main.rs
+++ etapa-1/src/main.rs
@@ -1,3 +1,62 @@
+struct KeyRequest {
+    key: String,
+    ver: u64,
+}
+
+struct KeyRange {
+    from: KeyRequest,
+    to: KeyRequest,
+}
+
+struct KeyValueRequest {
+    key: String,
+    val: String,
+}
+
+struct KeyValueVersionReply {
+    key: String,
+    val: String,
+}
+
+struct PutReply {
+    key: String,
+    old_val: String,
+    old_ver: u64,
+    ver: u64,
+}
+
+struct KeyValueStore;
+
+impl KeyValueStore {
+    fn Get(keyRequest: KeyRequest) -> KeyValueVersionReply {
+        KeyValueVersionReply {
+            key: String::from(""),
+            val: String::from(""),
+        }
+    }
+    
+    fn GetRange(keyRequest: KeyRange) -> KeyValueVersionReply {
+        KeyValueVersionReply {
+            key: String::from(""),
+            val: String::from(""),
+        }
+    }
+    
+    fn GetAll() {}
+    
+    fn Put() {}
+    
+    fn PutAll() {}
+
+    fn Del() {}
+    
+    fn DelRange() {}
+    
+    fn DelAll() {}
+    
+    fn Trim() {}
+}
+
 fn main() {
     println!("Hello, world!");
 }
