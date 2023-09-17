1a16307b63999ba85f70bbe93e65d102aeb568b3
Lucas Marçal Coutinho <lucasmc64@proton.me>
2023-09-17 16:48:05 -0300

    feat: add more required steps

--- etapa-1/compile.sh
+++ etapa-1/compile.sh
@@ -1,8 +1,36 @@
 #! /usr/bin/bash
 
-# falta comandos para instalar requisitos
+# Global variables
 
-# compila kvs.proto
-var=`python3 -m grpc_tools.protoc -Iprotos --python_out=src --pyi_out=src --grpc_python_out=src protos/kvs.proto`
-echo $var
+SCRIPT_DIRECTORY=$(pwd -P)
 
+# Create virtual environment
+
+if [ ! -d "${SCRIPT_DIRECTORY}/pyenv" ]; then
+  python3 -m venv pyenv
+fi
+
+# Activating virtual environment
+
+# shellcheck source=/dev/null
+source "${SCRIPT_DIRECTORY}/pyenv/bin/activate"
+
+# Upgrade Pip
+
+python3 -m pip install --upgrade pip
+
+# Install gRPC 
+
+python3 -m pip install grpcio
+
+# Install gRPC tools (plugin)
+
+python3 -m pip install grpcio-tools
+
+# Compile kvs.proto
+
+python3 -m grpc_tools.protoc -Iprotos --python_out=src --pyi_out=src --grpc_python_out=src protos/kvs.proto
+
+# Leave virtual environment
+
+deactivate
